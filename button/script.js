e82420ae9764409896e1b65d2d823a2977a2e784
Gartuz <gartuz@gmail.com>
2019-04-09 23:49:59 -0600

    Update Buttons Add
    Updating adding elements to Ordered Listo with a button to delete the element

--- button/script.js
+++ button/script.js
@@ -4,13 +4,24 @@ var ol = document.querySelector("ol");
 /* Append Item at Order Item List */
 
 function inputLength(){
-    
+
     return inputText.value.length;
 }
 
 function createListElement(){
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("Eliminar");
+    btn.appendChild(t);
+    btn.id="forListen";
+    btn.addEventListener("click",function(){
+          btn.parentElement.remove();
+
+    });
+
+
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(inputText.value));
+    li.appendChild(btn);
     ol.appendChild(li);
     inputText.value="";
 }
@@ -30,6 +41,13 @@ function addListAfterEnter(event){
 
 }
 
+function toggleAfterClick(){
+
+    ol.classList.toggle("done");
+}
+
 button.addEventListener("click", addListAfterClick);
 
 inputText.addEventListener("keypress", addListAfterEnter);
+
+ol.addEventListener("click", toggleAfterClick);
